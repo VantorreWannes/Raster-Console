44fc5c0dca87e4873132fdf15fce580f0d30b845
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2022-12-14 14:06:15 +0100

    Added refresh screen

--- Explain.md
+++ Explain.md
@@ -2,4 +2,5 @@
 - grid is made of x array containing y arrays
 - inverted y print
 - bottom left = 0,0
+- has a function to print ascii art in raster
 - ?dict containig sides? 
--- Main.py
+++ Main.py
@@ -1,3 +1,29 @@
+import numpy as np
+
+
+class Grid:
+    def __init__(self, WIDTH=0, HEIGHT=0):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+
+    def print_horizontal_slice(self):
+        '''Prints a horizontal slice of the array from left to right.'''
+        pass
+
+    def print_attributes(self):
+        '''Debug function; Prints the object's Width and Height.'''
+        print(self.WIDTH, self.HEIGHT)
+
+
+def refresh_screen(space):
+    '''Prints a bunch a newlines so the previous messages get pushed up.'''
+    for _ in range(space):
+        print()
+
+
 if __name__ == "__main__":
-    #MyGrid.printgrid()
+    MyGrid = Grid(10, 15)
+    MyGrid.print_attributes()
+    refresh_screen(0)
+    MyGrid.print_attributes()
     pass
