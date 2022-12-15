9de5a7217414fa1f40f863b0dfcdcc67e803e4eb
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2022-12-15 20:19:34 +0100

    Added check_picture_file and read_picture_file

--- Explain.md
+++ Explain.md
@@ -1,5 +1,5 @@
 ### Object containing grid
-- grid is made of x array containing y arrays
+- grid is made of width x array containing height y arrays
 - inverted y print
 - bottom left = 0,0
 - has a function to print ascii art in raster
--- Main.py
+++ Main.py
@@ -1,30 +1,33 @@
-import numpy as np
-
 
 class Grid:
     def __init__(self, WIDTH=0, HEIGHT=0):
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
+        self.screen = self.create_empty_grid()
 
     def print_grid():
-        ''''''
+        '''Prints every horizontal row of the grid under each other.'''
         pass
-    
-    def update_pixel(self, X, Y, new_character):
-        '''Changes the character behind a certain X and Y combination.'''
-        pass
-    
-    def create_empty_grid(self, width, height):
+
+    def create_empty_grid(self):
         '''Creates an array of X size containing multiple arrays of Y size.'''
         pass
 
-    def print_horizontal_slice(self):
-        '''Prints a horizontal slice of the array from left to right.'''
+    def update_pixel(self, X, Y, new_character):
+        '''Changes the character behind a certain X and Y combination.'''
         pass
 
     def print_attributes(self):
         '''Debug function; Prints the object's Width and Height.'''
         print(self.WIDTH, self.HEIGHT)
+
+    def check_picture_file(self, file):
+        '''Checks if the txt file's text has the same width and height as our Grid instance.'''
+        pass
+
+    def read_picture_file(self, file):
+        '''Converts a txt file containing ascii art into nested arrays containing char values.'''
+        pass
 
 
 def push_up(space):
