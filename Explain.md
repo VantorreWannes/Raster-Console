21b2cc726fd1b9b8739178eae29abfe2704f2567
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2022-12-15 21:12:32 +0100

    End oF Session

--- Explain.md
+++ Explain.md
@@ -1,5 +1,7 @@
 ### Object containing grid
 - grid is made of width x array containing height y arrays
+- row = vorizontal
+- coloumn = vertical
 - inverted y print
 - bottom left = 0,0
 - has a function to print ascii art in raster
--- Main.py
+++ Main.py
@@ -11,11 +11,20 @@ class Grid:
 
     def print_grid(self):
         '''Prints every horizontal row of the grid under each other.'''
-        pass
+        for y in range(self.HEIGHT): 
+            # Loop over every row saving the current level as y
+            row = list() 
+            # Initialise our list acting as a collection of all the characters on a single horizontal slice.
+            for coloumn in self.screen:
+                row.append(coloumn[y]) 
+            # Loop over every value containing a list in self.screen and saving the y'th value into the row collection
+            print(*row, sep = "")
+            # Print every value in
 
     def create_empty_grid(self):
         '''Creates an array of X size containing multiple arrays of Y size.'''
-        return [[' '] * self.HEIGHT] * self.WIDTH # Initialise the main width array every slot stores a vertical row of the grid
+        return [['*'] * self.HEIGHT] * self.WIDTH 
+        # Initialise the main width array every slot stores a vertical row of the grid.
 
     def update_pixel(self, X, Y, new_character):
         '''Changes the character behind a certain X and Y combination.'''
@@ -37,6 +46,6 @@ def push_up(space):
 
 
 if __name__ == "__main__":
-    MyGrid = Grid(4, 4)
-    print(MyGrid.screen)
+    MyGrid = Grid(12, 4)
+    MyGrid.print_grid()
     pass
