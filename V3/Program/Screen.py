4a1ec9d7ed6ed5db42ab3f5a6e55e880b3e8aacb
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-04-11 15:57:29 +0200

    Merge shrink() and grow() into resize()

--- V3/Program/Screen.py
+++ V3/Program/Screen.py
@@ -1,4 +1,4 @@
-class Screen:
+class Screen(object):
     def __init__(self, name, size) -> None:
         self.name = name
         self.size = size
@@ -8,18 +8,15 @@ class Screen:
     def clear(self):
         self.screen = {coords: " " for coords in self.screen}
     
-    def shrink(self, new_size):
-        return {coords: item for coords, item in self.screen.items() if coords < (new_size, new_size*3) }
-    
-    def grow(self, new_size):
+    def resize(self, new_size):
         self.screen = {(y, x): self.screen.get((y, x), " ") for y in range(new_size-1, -1, -1) for x in range(new_size*3)}
 
 
 if __name__ == "__main__":
-    SCREEN = Screen("Screen one", 2-1)
+    SCREEN = Screen("Screen one", 2)
     print("SIZE 2 SCREEN: \n", *SCREEN.screen, sep="\n")
-    SCREEN.grow(4-1)
+    SCREEN.resize(2)
     print("SIZE 4 SCREEN: \n", *SCREEN.screen, sep="\n")
-    SCREEN.shrink(3-1)
+    SCREEN.resize(3)
     print("SIZE 3 SCREEN: \n", *SCREEN.screen, sep="\n")
 
